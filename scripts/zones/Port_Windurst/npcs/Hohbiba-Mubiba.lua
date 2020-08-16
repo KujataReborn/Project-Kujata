41ca780d565e796528ac0e133a61af2ff555a30a
BillBows <william.henrique@outlook.com>
2020-08-16 14:15:56 -0300

    Vendors: Port Windurst

--- scripts/zones/Port_Windurst/npcs/Hohbiba-Mubiba.lua
+++ scripts/zones/Port_Windurst/npcs/Hohbiba-Mubiba.lua
@@ -13,18 +13,21 @@ function onTrigger(player, npc)
     local stock =
     {
         17051,  1440, 1,    -- Yew Wand
+        17059,    90, 1,    -- Bronze Rod
         17090,  3642, 1,    -- Elm Staff
         17097, 18422, 1,    -- Elm Pole
+
+        17050,   340, 2,    -- Willow Wand
         17026,  4945, 2,    -- Bone Cudgel
+        17089,   584, 2,    -- Holly Staff
         17096,  4669, 2,    -- Holly Pole
+
         17049,    47, 3,    -- Maple Wand
-        17050,   340, 3,    -- Willow Wand
-        17059,    91, 3,    -- Bronze Rod
         17024,    66, 3,    -- Ash Club
         17025,  1600, 3,    -- Chestnut Club
         17088,    58, 3,    -- Ash Staff
-        17089,   584, 3,    -- Holly Staff
         17095,   386, 3,    -- Ash Pole
+        17059,    91, 3,    -- Bronze Rod
     }
 
     player:showText(npc, ID.text.HOHBIBAMUBIBA_SHOP_DIALOG)
