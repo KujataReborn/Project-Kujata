0c5c9ea6fcd8915ce2baf1369b02a665b53e247b
BillBows <william.henrique@outlook.com>
2020-08-16 14:18:36 -0300

    Vendors: Southern San d'Oria

--- scripts/zones/Southern_San_dOria/npcs/Ashene.lua
+++ scripts/zones/Southern_San_dOria/npcs/Ashene.lua
@@ -19,9 +19,12 @@ function onTrigger(player, npc)
         16545, 21067, 1,    -- Broadsword
         16576, 35769, 1,    -- Hunting Sword
         16524, 13406, 1,    -- Fleuret
+
         16450,  1827, 2,    -- Dagger
         16536,  7128, 2,    -- Iron Sword
         16566,  8294, 2,    -- Longsword
+
+        16385,   129, 3,    -- Cesti
         16448,   140, 3,    -- Bronze Dagger
         16449,   837, 3,    -- Brass Dagger
         16531,  3523, 3,    -- Brass Xiphos
