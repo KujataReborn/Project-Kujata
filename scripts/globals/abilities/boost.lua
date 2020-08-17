49c4df12af3127db26a69f770e46d8e3fc5f760b
BillBows <william.henrique@outlook.com>
2020-08-17 16:12:07 -0300

    Monk: Boost
    - Formatting

--- scripts/globals/abilities/boost.lua
+++ scripts/globals/abilities/boost.lua
@@ -2,8 +2,8 @@
 -- Ability: Boost
 -- Enhances user's next attack.
 -- Obtained: Monk Level 5
--- Recast Time: 0:15
--- Duration: 3:00
+-- Recast Time: 0:00:15
+-- Duration: 0:03:00
 -----------------------------------
 require("scripts/globals/status")
 -----------------------------------
@@ -19,8 +19,9 @@ function onUseAbility(player, target, ability)
 
     local power = 12.5 + (0.10 * player:getMod(tpz.mod.BOOST_EFFECT))
 
-    if (player:hasStatusEffect(tpz.effect.BOOST) == true) then
+    if player:hasStatusEffect(tpz.effect.BOOST) then
         local effect = player:getStatusEffect(tpz.effect.BOOST)
+
         effect:setPower(effect:getPower() + power)
         player:addMod(tpz.mod.ATTP, power)
     else
