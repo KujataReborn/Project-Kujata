8fe5fdd90de4849ecfe1a6850b6b2206adff6a80
Djarum <39584088+KujataReborn@users.noreply.github.com>
2020-10-23 18:35:29 -0700

    Steam Cleaner: Adding additional effect TP Drain on hit

--- scripts/zones/VeLugannon_Palace/mobs/Steam_Cleaner.lua
+++ scripts/zones/VeLugannon_Palace/mobs/Steam_Cleaner.lua
@@ -2,8 +2,24 @@
 -- Area: Ve'Lugannon Palace
 --   NM: Steam Cleaner
 -----------------------------------
+require("scripts/globals/msg")
+require("scripts/globals/status")
+-----------------------------------
 
-function onMobDeath(mob, player, isKiller)
+function onMobInitialize(mob)
+    mob:setMobMod(tpz.mobMod.ADD_EFFECT, 1)
+end
+
+function onAdditionalEffect(mob,target,damage)
+    if math.random(100) <= 10 then
+        local drain = math.random(15, 30)
+        
+        target:delTP(drain)
+
+        return tpz.subEffect.TP_DRAIN, tpz.msg.basic.ADD_EFFECT_TP_DRAIN, drain
+    else
+        return 0, 0, 0
+    end
 end
 
 function onMobDespawn(mob)
