bdbc96be3c666bd0093cddd0dcb0286fb0422e3c
Djarum <39584088+KujataReborn@users.noreply.github.com>
2020-10-23 20:27:01 -0700

    Ollas: Adding additionl effect dispel on hit

--- scripts/zones/The_Shrine_of_RuAvitau/mobs/Olla_Grande.lua
+++ scripts/zones/The_Shrine_of_RuAvitau/mobs/Olla_Grande.lua
@@ -3,12 +3,26 @@
 --  Mob: Olla Grande
 -----------------------------------
 local ID = require("scripts/zones/The_Shrine_of_RuAvitau/IDs")
+require("scripts/globals/msg")
 require("scripts/globals/settings")
 require("scripts/globals/status")
 -----------------------------------
 
 function onMobInitialize(mob)
+    mob:setMobMod(tpz.mobMod.ADD_EFFECT, 1)
     mob:setMobMod(tpz.mobMod.IDLE_DESPAWN, 120)
+end
+
+function onAdditionalEffect(mob, target, damage)
+    if math.random(100) <= 15 then
+        local effect = target:dispelStatusEffect()
+
+        if effect ~= tpz.effect.NONE then
+            return tpz.subEffect.DISPEL, tpz.msg.basic.ADD_EFFECT_DISPEL, effect
+        end
+    end
+
+    return 0, 0, 0
 end
 
 function onMobDeath(mob, player, isKiller)
