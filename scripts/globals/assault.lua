cb58216a4d61e94097ec113433a19b49fb837c92
BillBows <william.henrique@outlook.com>
2020-11-17 17:46:52 -0300

    Assault global (mission enums)

---
+++ scripts/globals/assault.lua
@@ -0,0 +1,65 @@
+---------------------------------------
+--
+-- Assault
+--
+---------------------------------------
+tpz = tpz or {}
+tpz.assault = tpz.assault or {}
+---------------------------------------
+
+tpz.assault.missions =
+{
+    LEUJAOAM_CLEANSING                =  1,
+    ORICHALCUM_SURVEY                 =  2,
+    ESCORT_PROFESSOR_CHANOIX          =  3,
+    SHANARHA_GRASS_CONSERVATION       =  4,
+    COUNTING_SHEEP                    =  5,
+    SUPPLIES_RECOVERY                 =  6,
+    AZURE_EXPERIMENTS                 =  7,
+    IMPERIAL_CODE                     =  8,
+    RED_VERSUS_BLUE                   =  9,
+    BLOODY_RONDO                      = 10,
+    IMPERIAL_AGENT_RESCUE             = 11,
+    PREEMPTIVE_STRIKE                 = 12,
+    SAGELORD_ELIMINATION              = 13,
+    BREAKING_MORALE                   = 14,
+    THE_DOUBLE_AGENT                  = 15,
+    IMPERIAL_TREASURE_RETRIEVAL       = 16,
+    BLITZKRIEG                        = 17,
+    MARIDS_IN_THE_MIST                = 18,
+    AZURE_AILMENTS                    = 19,
+    THE_SUSANOO_SHUFFLE               = 20,
+    EXCAVATION_DUTY                   = 21,
+    LEBROS_SUPPLIES                   = 22,
+    TROLL_FUGITIVES                   = 23,
+    EVADE_AND_ESCAPE                  = 24,
+    SIEGEMASTER_ASSASSINATION         = 25,
+    APKALLU_BREEDING                  = 26,
+    WAMOURA_FARM_RAID                 = 27,
+    EGG_CONSERVATION                  = 28,
+    OPERATION__BLACK_PEARL            = 29,
+    BETTER_THAN_ONE                   = 30,
+    SEAGULL_GROUNDED                  = 31,
+    REQUIEM                           = 32,
+    SAVING_PRIVATE_RYAAF              = 33,
+    SHOOTING_DOWN_THE_BARON           = 34,
+    BUILDING_BRIDGES                  = 35,
+    STOP_THE_BLOODSHED                = 36,
+    DEFUSE_THE_THREAT                 = 37,
+    OPERATION__SNAKE_EYES             = 38,
+    WAKE_THE_PUPPET                   = 39,
+    THE_PRICE_IS_RIGHT                = 40,
+    GOLDEN_SALVAGE                    = 41,
+    LAMIA_NO_13                       = 42,
+    EXTERMINATION                     = 43,
+    DEMOLITION_DUTY                   = 44,
+    SEARAT_SALVATION                  = 45,
+    APKALLU_SEIZURE                   = 46,
+    LOST_AND_FOUND                    = 47,
+    DESERTER                          = 48,
+    DESPERATELY_SEEKING_CEPHALOPODS   = 49,
+    BELLEROPHON_S_BLISS               = 50,
+    NYZUL_ISLE_INVESTIGATION          = 51,
+    NYZUL_ISLE_UNCHARTED_AREA_SURVEY  = 52,
+}
+
